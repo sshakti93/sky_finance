1eac34c9cc487cbe3e7d3e4bedfa8d18c3246d16
sshakti93 <siddhishakti31@gmail.com>
2024-06-12 19:08:51 +0530

    feat: imports the header to be statically displayed throughout the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Headerpage from "./Components/Headerpage/Headerpage";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Headerpage/>
+        {children}
+        </body>
     </html>
   );
 }
