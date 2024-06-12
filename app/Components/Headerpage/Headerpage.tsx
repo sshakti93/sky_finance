69ac2ee08e895ae040f93b2b84aa5c3c76d76688
sshakti93 <siddhishakti31@gmail.com>
2024-06-12 19:10:03 +0530

    style: styling and formatting of the header

---
+++ app/Components/Headerpage/Headerpage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Headerpage.css"
+import Image from 'next/image'
+export default function Headerpage() {
+  return (
+    <div>
+      <div className="header-center">
+        <div className="full-header">
+            <div className="site-logo">
+              <Image src={"/images/sitelogo.svg"} alt='sky_finance' width={190} height={40}/>
+            </div>
+            <div className="touch-button">
+              <button>Get in touch</button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
