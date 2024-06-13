c951e23a5dca067d5c8050b12bbd089b2901cc27
sshakti93 <siddhishakti31@gmail.com>
2024-06-13 20:21:51 +0530

    feat: page for the terms of use of sky financial

---
+++ app/pages/Tos/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Tos.css"
+export default function page() {
+  return (
+    <div>
+        <div className="home-nav"></div>
+        <div className="tos-title"></div>
+        <div className="tos-desc"></div>
+    </div>
+  )
+}
+
