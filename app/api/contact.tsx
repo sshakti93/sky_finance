31626a634266effa7792d252c30170a00f53ee9f
sshakti93 <siddhishakti31@gmail.com>
2024-06-15 13:32:06 +0530

    feat: api file to store data from the user

---
+++ app/api/contact.tsx
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from "nodemailer";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { name, email, subject, phone, message } = req.body as {
+      name: string;
+      email: string;
+      subject: string;
+      phone: string;
+      message: string;
+    };
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: process.env.GMAIL_USER,
+      subject: `Contact Form Submission: ${subject}`,
+      text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: "Email sent successfully!" });
+    } catch (error) {
+      console.error("Error sending email:", (error as Error).message);
+      if ((error as any).response) {
+        console.error("SMTP Error Response:", (error as any).response);
+      }
+      res.status(500).json({ message: "Error sending email", error });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
