b2f2e52b3c6bf42cf919463175a985e261c4e426
sshakti93 <siddhishakti31@gmail.com>
2024-06-15 15:10:29 +0530

    misc

--- app/Components/Headerpage/Headerpage.tsx
+++ app/Components/Headerpage/Headerpage.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import "./Headerpage.css"
-import Image from 'next/image'
+import React from "react";
+import "./Headerpage.css";
+import Image from "next/image";
 export default function Headerpage() {
   return (
     <div>
       <div className="header-center">
         <div className="full-header">
-            <div className="site-logo">
-              <Image src={"/images/sitelogo.svg"} alt='sky_finance' width={190} height={40}/>
-            </div>
-            <div className="touch-button">
-              <button>Get in touch</button>
-            </div>
+          <div className="site-logo">
+            <Image
+              src={"/images/sitelogo.svg"}
+              alt="sky_finance"
+              width={190}
+              height={40}
+            />
+          </div>
+          <div className="touch-button">
+            <button>Get in touch</button>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
